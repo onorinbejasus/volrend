3e52051a6e886f6e4e7f71964c1b5b33e723c3cb
Tim Luciani <onorinbejasus@gmail.com>
2015-10-23 09:40:00 -0500

    fixed the fragment shader

--- volumerenderer.fs
+++ volumerenderer.fs
@@ -63,22 +63,11 @@ void main()
 
       if (alpha > .003) {
 
-	vec4  norm = vec4(texture3D(normalTex, rayPos));
+      	vec4  norm = vec4(texture3D(normalTex, rayPos));
 
-	float magDiff = norm.a * 8.0;
-	vec3  N = norm.xyz;
+      	float magDiff = norm.a * 8.0;
+      	vec3  N = norm.xyz;
 
-<<<<<<< HEAD
-	vec3  cmapColor = vec3(texture1D(cmapTex, scalar));
-
-	float diffuseLight = max(dot(L, N), 0.0);
-	vec3  diffuse      = cmapColor * diffuseLight; // also should mult by Kd, but it's 1.0
-
-	float specularLight = pow(max(dot(H, N), 0.0), n);
-	vec3  specular      = vec3(specularLight); // also should mult by Ks, but it's 1.0
-
-	vec3 ambient = ambientLight; // also should mult by Ka, but it's 1.0
-=======
       	vec4 lower_cmapColor = vec4(0.0);
 
         if(scalar >= 25500.0 && scalar <= 26500.0)
@@ -98,14 +87,10 @@ void main()
         else{
           lower_cmapColor = mix( texture1D(cmapTex, 28500.0), texture1D(cmapTex, 65535.0), scalar );
         }
->>>>>>> 3c4e4e2955341c7943f3ded82b431c5458bb9efb
 
-	alpha *= sampleRate;
-	alpha = min(alpha, 1.0);
+      	float diffuseLight = max(dot(L, N), 0.0);
+      	vec3  diffuse      = vec3(lower_cmapColor * diffuseLight); // also should mult by Kd, but it's 1.0
 
-<<<<<<< HEAD
-	rayColor += (1.0-rayColor.a) * vec4(((ambient + diffuse + specular)*alpha*magDiff), alpha*magDiff);
-=======
       	float specularLight = pow(max(dot(H, N), 0.0), n);
       	vec3  specular      = vec3(specularLight); // also should mult by Ks, but it's 1.0
 
@@ -118,7 +103,6 @@ void main()
 
       	rayColor += (1.0-rayColor.a) * vec4(((ambient + diffuse + specular)*alpha*magDiff), alpha*magDiff);
 
->>>>>>> 3c4e4e2955341c7943f3ded82b431c5458bb9efb
       }
 
       rayPos += rayStep;
